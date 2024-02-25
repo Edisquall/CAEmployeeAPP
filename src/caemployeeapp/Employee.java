02a12c6827193223bad04417ea853a077e1a7b86
Edisquall <152781383+Edisquall@users.noreply.github.com>
2024-02-25 16:22:04 +0000

    Redoing constructor
    Redoing constructor because it appear to not work

--- src/caemployeeapp/Employee.java
+++ src/caemployeeapp/Employee.java
@@ -16,25 +16,7 @@ public class Employee {
     
     private static int nextEmpNum = 1;
 
-    //Contructor
-    public Employee(String name, String email, int empNum) {
-        this.name = name;
-        this.email = email;
-        this.empNum = empNum;
-    }
-    public String getName() {
-        return name;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public int getEmpNum() {
-        return empNum;
-    }
-    //Getting nextEMpNum
-   public static int getNextEmpNum() {
-       return nextEmpNum;
-   }
+
    //validating email
     public void validateEmail (String email) {
         if (email.length() >3) {
