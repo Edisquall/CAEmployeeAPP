fd10c1e36a3ce5ebbb75fc3e5b2ee38911774263
Edisquall <152781383+Edisquall@users.noreply.github.com>
2024-02-25 17:02:36 +0000

    Company
    Starting company contructors with default value

--- src/caemployeeapp/CAEmployeeAPP.java
+++ src/caemployeeapp/CAEmployeeAPP.java
@@ -1,4 +1,4 @@
- /*
+/*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
@@ -15,25 +15,25 @@ public class CAEmployeeAPP {
      */
     public static void main(String[] args) {
         //Creating 3 employee objects
-        
+
         Employee employee1 = new Employee("Joe Bloggs", "jb@gmail.com");
         Employee employee2 = new Employee("Ann Banana", "ab@gmail.com");
         Employee employee3 = new Employee("Tom Thumb", "tt@gmail.com");
-        
+
         //Array for employee
-        Employee[] projectgroup = {employee1, employee2, employee3};
-        
+        Employee[] projectGroup = {employee1, employee2, employee3};
+
         //Priting value of nextEmpNum
-        
-        System.out.println("Value of NextEmpNum: " +Employee.getNextEmpNum());
-        
+        System.out.println("Value of NextEmpNum: " + Employee.getNextEmpNum());
+
         //Displaying name of each employee
-        
         int m = 1;
         System.out.println("Here it is all the employees with the empNUm above " + m + ":");
-        if (Employee.getEmpNum()> m) {
-            System.out.println(eEmployee.getName());
+        for (Employee empLoop : projectGroup) {
+            if (empLoop.getEmpNum() > m) {
+                System.out.println(empLoop.getName());
+            }
         }
     }
-    
+
 }
---
+++ src/caemployeeapp/Company.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package caemployeeapp;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Edi
+ */
+public class Company {
+    private String companyName;
+    private ArrayList <String> staff;
+    
+    // Constructor which initialises all fields
+
+    public Company() {
+        this.companyName = "Default Company";
+        this.staff = "Default Staff";
+    }
+    
+    
+}
