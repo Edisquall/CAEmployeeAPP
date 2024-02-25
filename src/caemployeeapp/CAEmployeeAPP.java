c65f7ad8860f27a1be5eeb2c64c4137ae847a6ae
Edisquall <152781383+Edisquall@users.noreply.github.com>
2024-02-25 16:37:09 +0000

    Getters and constructor
    Fixed and deleting new java class I created for test

--- src/caemployeeapp/CAEmployeeAPP.java
+++ src/caemployeeapp/CAEmployeeAPP.java
@@ -15,9 +15,10 @@ public class CAEmployeeAPP {
      */
     public static void main(String[] args) {
         //Creating 3 employee objects
+        
         Employee employee1 = new Employee("Joe Bloggs", "jb@gmail.com");
-        
-        
+        Employee employee2 = new Employee("Ann Banana", "ab@gmail.com");
+        Employee employee3 = new Employee("Tom Thumb", "tt@gmail.com");
         
     }
     
--- src/caemployeeapp/Employee.java
+++ src/caemployeeapp/Employee.java
@@ -9,21 +9,52 @@ package caemployeeapp;
  * @author Edi
  */
 public class Employee {
+
     //Information from the users
     private String name;
     private String email;
     private int empNum;
-    
+
     private static int nextEmpNum = 1;
 
+    //Contructor with default values
+    public Employee() {
+        this.name = "name";
+        this.email = "email@gmail.com";
+        this.empNum = nextEmpNum++;
+    }
+// Contructor with parameters
+    public Employee(String name, String email) {
+        this.name = name;
+        this.email = email;
+        this.empNum = nextEmpNum++;
+    }
+    
+//setting up getters
 
-   //validating email
-    public void validateEmail (String email) {
-        if (email.length() >3) {
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getEmpNum() {
+        return empNum;
+    }
+
+    public static int getNextEmpNum() {
+        return nextEmpNum;
+    }
+
+    //validating email
+    public void validateEmail(String email) {
+        if (email.length() > 3) {
             this.email = email;
         } else {
             System.out.println("Email must contain 3 or more characters.");
         }
-        
+
     }
 }
